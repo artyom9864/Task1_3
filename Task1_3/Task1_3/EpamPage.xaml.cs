f6c937ff1bb84872491eed6c110a4f6f43c8ea9c
Артём Братчук <artyom9864@mail.ru>
2021-01-05 20:48:31 +0300

    Добавьте файлы проекта.

---
+++ Task1_3/Task1_3/EpamPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using StringLibrary;
+
+namespace Task1_3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EpamPage : ContentPage
+	{
+		public EpamPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ButtonHello_Click (object sender, EventArgs e)
+        {
+            var helloMessage = new HelloMessage();
+            var message = helloMessage.GetHelloMessage(_entryName.Text);
+
+            _lableMessage.Text = helloMessage.GetHelloMessage(_entryName.Text);
+        }
+
+
+    }
+}
---
+++ Task1_3/Task1_3/EpamTask.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Task1_3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EpamTask : ContentView
+	{
+		public EpamTask ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
